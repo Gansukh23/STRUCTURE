2a07e2c3e74e95fad3a77781ea0b31491001484a
Gnskh <lhagva.gansuh99@gmail.com>
2020-05-08 08:48:08 +0800

    first commit

---
+++ STRUCTURE.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+mkdir -p ASSET/Img 
+mkdir CONTEXT
+mkdir -p  CONSTANTS/Locale
+mkdir -p  CONSTANTS/Url
+mkdir -p  CONSTANTS/Env
+mkdir -p SERVICE/RestAPI
+mkdir -p SERVICE/Helpers
+mkdir PAGES
+mkdir ROUTES
+mkdir SHARED_COMPONENTS
